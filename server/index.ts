81356206513cd4455d9a231867b1a0a25c524fde
Eden Chan <edenchan717@gmail.com>
2025-01-24 22:55:33 -0700

    load env properly

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { config } from "dotenv";
+config(); // Load environment variables
 
 const app = express();
 app.use(express.json());
